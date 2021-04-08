8a1b35e45f9609efd4a2d5a5e7271a11de09647b
Reuben Smith <reuben-smith@hotmail.com>
2021-04-08 14:19:49 +0930

    Adds fetch support for Internet Explorer browser

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "whatwg-fetch"; // Adds fetch support for IE browser.
 
 /** Import the main React Application */
 import App from "./App";
